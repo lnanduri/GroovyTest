b9ed56f6063a5d1402aed21304b0e0da32a5df74
lnanduri <64898588+lnanduri@users.noreply.github.com>
2022-02-16 00:08:10 +0530

    added new file

---
+++ notify.groovy
@@ -0,0 +1,17 @@
+def notifyStarted() {
+  // send to Slack
+  //slackSend (color: '#FFFF00', message: "STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
+
+  // send to HipChat
+  //hipchatSend (color: 'YELLOW', notify: true,
+      message: "STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})"
+    )
+
+  // send to email
+  emailext (
+      subject: "STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+      body: """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+        <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""",
+      recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+    )
+}
